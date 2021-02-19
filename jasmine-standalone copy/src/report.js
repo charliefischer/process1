1ae9f3cf8b06a46eb3d75200ef8f4ab7d7004f32
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-19 10:56:06 +0000

    passwed tests

--- jasmine-standalone copy/src/report.js
+++ jasmine-standalone copy/src/report.js
@@ -6,6 +6,8 @@ class Report {
   results = (str) =>{
     let countedResults = {}
     let arrayOfResults = str.split(',')
+    let uniqueArr = arrayOfResults.filter(onlyUnique)
+    let answer = ''
     let i;
     for (i = 0; i < arrayOfResults.length; i++){
       if(countedResults.hasOwnProperty(arrayOfResults[i])){
@@ -13,18 +15,20 @@ class Report {
       } else {
         countedResults[arrayOfResults[i]] = 1;
       }
-      // console.log(`${arrayOfResults[i]}: ${countedResults[arrayOfResults[i]]}`)
-    }
-    console.log(i)
-    console.log(arrayOfResults[i - 1])
-    console.log(countedResults[arrayOfResults[i]])
-    if(arrayOfResults.length == 1) {
-      return `${arrayOfResults[i - 1]}: ${countedResults[arrayOfResults[i - 1]]}`
-    } else {
-      for(let n = 0; n < arrayOfResults.length; n++) {
-        return `${arrayOfResults[n]}: ${countedResults[arrayOfResults[n]]}`
-      }
+   }
+
+    function onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
     }
 
+    if(uniqueArr.length == 1) {
+      return `${arrayOfResults[i - 1]}: ${countedResults[arrayOfResults[i - 1]]}`
+    } else {
+      answer = `${uniqueArr[0]}: ${countedResults[uniqueArr[0]]}`
+      for(let n = 1; n < uniqueArr.length; n++) {
+        answer += '\n' + `${uniqueArr[n]}: ${countedResults[uniqueArr[n]]}`
+      }
+      return answer
+    }
   }
 }
